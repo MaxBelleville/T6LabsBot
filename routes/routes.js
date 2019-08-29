7575fe406c04fc25b2e684f242abca66d1cd0a52
Max <max.belleville@hotmail.com>
2019-08-28 21:45:20 -0400

    FUCK

--- routes/routes.js
+++ routes/routes.js
@@ -23,4 +23,5 @@ module.exports = function (app) {
         .post('/commands/clear', commands.clear)
         .post('/commands/poll', commands.poll)
         .post('/commands/task',commands.task)
+	.get('/ouath',commands.ouath)
 };
