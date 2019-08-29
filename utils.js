52b1c8e123ba8f699ec7408b201107375ad43321
Max <max.belleville@hotmail.com>
2019-08-28 21:06:05 -0400

    Changed oauth

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 //Utility functions to simplify reading from slack
 const {WebClient} = require('@slack/web-api');
-const token = process.env.SLACK_AUTH_TOKEN;
+const token = process.env.SLACK_TEST_TOKEN;
 const web = new WebClient(token);
 module.exports = {
     getWeb() {
