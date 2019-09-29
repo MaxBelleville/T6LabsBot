b3f88bce94df53638c020a24f8c2ae9776880dc1
MaxBelleville <max.belleville@hotmail.com>
2019-09-29 13:52:55 -0400

    Hopefully fixed it

--- botStart.js
+++ botStart.js
@@ -6,14 +6,18 @@ const gitReader =require('./controllers/gitReader')
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 80;
-
+const http = require("http");
 mongoose.connect(process.env.DB_URI,
   { useNewUrlParser: true })
 app.use(cors());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+setInterval(function() {
+    http.get("https://t6bot.herokuapp.com");
+}, 300000);
+
 app.use(bodyParser.json());
 require('./routes/routes')(app);
 app.listen(port,()=>{
-gitReader.start();});
+gitReader.start(); });
